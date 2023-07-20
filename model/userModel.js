49268710c7dcc9618067f47d48898e3d20518755
Ankur Verma <cwp.ankur@gmail.com>
2023-07-20 17:22:51 +0530

    adfd

--- model/userModel.js
+++ model/userModel.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const userModel = new mongoose.Schema({
     name: {
         type: String
+    },
+    usernmae:{
+        type: String
     }
 }, { timestamps: true });
 
