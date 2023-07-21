8d69271b4a1ec6f9ea462197b82331433bc00f83
Ankur Verma <cwp.ankur@gmail.com>
2023-07-21 13:16:24 +0530

    last commit ankur

--- model/userModel.js
+++ model/userModel.js
@@ -16,6 +16,12 @@ const userModel = new mongoose.Schema({
     mobile:{
         type:Number
     },
+    book:{
+        type: String 
+    },
+    mobile:{
+        type:Number
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model("user",userModel);
