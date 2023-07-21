24203d86551dfa1f3dcffdf2afc03feb2c8c2d41
Ankur Verma <cwp.ankur@gmail.com>
2023-07-21 19:57:21 +0530

    first commit

--- model/userModel.js
+++ model/userModel.js
@@ -16,6 +16,12 @@ const userModel = new mongoose.Schema({
     mobile:{
         type:Number
     },
+    password:{
+        type: String
+    },
+    mobile:{
+        type:Number
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model("user",userModel);
